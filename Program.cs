316ad269e17efe117c92dc0cb1625f9ef9d07a76
ScStew <scottmstewart2@gmail.com>
2020-10-16 22:18:21 -0400

    Added example of overflow

--- Program.cs
+++ Program.cs
@@ -24,6 +24,9 @@ namespace C__intro
             int max = int.MaxValue;
             int min = int.MinValue;
             Console.WriteLine($"The range of integers is {min} to {max}");
+
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
         }
     }
 }
