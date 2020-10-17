08d9c4e52763bb80b7718d54914f505318d4b490
ScStew <scottmstewart2@gmail.com>
2020-10-16 22:02:54 -0400

    Added code to test Contains method

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ namespace C__intro
             string message = "Hello";
             string name = "Mike";
             Console.WriteLine($"{message} {name}");
+
+            string songLyrics = "You say goodbye, and I say hello";
+            Console.WriteLine(songLyrics.Contains("goodbye"));
+            Console.WriteLine(songLyrics.Contains("greetings"));
         }
     }
 }
